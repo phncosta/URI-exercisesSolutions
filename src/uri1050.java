ea9ed80ba2252822b0645a9dbff131240cf848a2
Paulo Henrique <phncosta@outlook.com>
2019-09-27 13:32:15 -0300

    Update Exercicios

--- src/uri1050.java
+++ src/uri1050.java
@@ -6,28 +6,38 @@ public class uri1050 {
 		Scanner input = new Scanner(System.in);
 
 		int ddd = input.nextInt();
+		String cidade;
 
-		if (ddd == 61) {
-			System.out.println("Brasilia");
-		} else if (ddd == 71) {
-			System.out.println("Salvador");
-		} else if (ddd == 11) {
-			System.out.println("Sao Paulo");
-		} else if (ddd == 21) {
-			System.out.println("Rio de Janeiro");
-		} else if (ddd == 32) {
-			System.out.println("Juiz de Fora");
-		} else if (ddd == 19) {
-			System.out.println("Campinas");
-		} else if (ddd == 27) {
-			System.out.println("Vitoria");
-		} else if (ddd == 31) {
-			System.out.println("Belo Horizonte");
-		} else {
-			System.out.println("DDD nao cadastrado");
+		switch (ddd) {
+		case 61:
+			cidade = "Brasilia";
+			break;
+		case 71:
+			cidade = "Salvador";
+			break;
+		case 11:
+			cidade = "Sao Paulo";
+			break;
+		case 21:
+			cidade = "Rio de Janeiro";
+			break;
+		case 32:
+			cidade = "Juiz de Fora";
+			break;
+		case 19:
+			cidade = "Campinas";
+			break;
+		case 27:
+			cidade = "Vitoria";
+			break;
+		case 31:
+			cidade = "Belo Horizonte";
+			break;
+		default:
+			cidade = "DDD nao cadastrado";
 		}
+		System.out.println(cidade);
 
 		input.close();
 	}
-
 }
